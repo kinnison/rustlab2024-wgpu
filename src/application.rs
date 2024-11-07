5df1b2bcf3a6501e9461cf96ec0f2ffaf1826da5
Niklas Korz <niklas@niklaskorz.de>
2024-11-07 15:03:30 +0100

    Chapter 2 template

--- src/application.rs
+++ src/application.rs
@@ -1,47 +1,39 @@
 // The tasks for this chapter are split into the different methods of Application.
-// Go through the methods from top to bottom.
-// Once all your methods are fully implemented, start your application and make sure
-// it displays two white triangles.
-// You can of course already try running your application inbetween to ensure no
-// validation errors are raised.
-// Afterwards, continue with adjusting your shaders in `application.wgsl`.
-//
 // Refer to https://docs.rs/wgpu/latest/wgpu/ to learn about a type's constructor,
 // methods and attributes.
 use std::sync::Arc;
 
 use anyhow::{Context, Result};
-use wgpu::RenderPipeline;
-use winit::{dpi::PhysicalSize, window::Window};
+use cgmath::{Vector3, Zero};
+use wgpu::{BindGroup, BindGroupLayout, RenderPipeline};
+use winit::{
+    dpi::PhysicalSize,
+    event::{ElementState, MouseButton, MouseScrollDelta, WindowEvent},
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window,
+};
+
+use crate::{arcball::CameraOperation, scene::Scene};
 
 pub struct Application {
     surface_config: wgpu::SurfaceConfiguration,
     surface: wgpu::Surface<'static>,
     device: wgpu::Device,
     queue: wgpu::Queue,
+    scene: Scene,
+    // render_bind_group_layout: BindGroupLayout,
+    // render_bind_group: BindGroup,
     render_pipeline: RenderPipeline,
+    mouse_down: bool,
 }
 
 impl Application {
     pub async fn new(window: Arc<Window>, size: PhysicalSize<u32>) -> Result<Self> {
-        // 1. We first must create a `wgpu::Instance`.
-        // This is the entrypoint to all communication with wgpu.
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::PRIMARY,
             ..Default::default()
         });
-
-        // 2. Next, we create our surface through the instance we created above.
-        // For this, we must pass a window for the surface to target.
-        // A surface is what anything we draw will be displayed on.
         let surface = instance.create_surface(window.clone())?;
-
-        // 3. Once we have our surface, we request an adapter that is compatible with
-        // this surface from our wgpu instance.
-        // We want to request a high performance GPU so in case our device is a laptop
-        // with two GPUs, we get the more powerful one.
-        // Note that reqeusting an adapter is an asynchronous operation that must be awaited.
-        // If no adapter matches our request options, we receive `None`.
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
                 power_preference: wgpu::PowerPreference::HighPerformance,
@@ -51,16 +43,6 @@ impl Application {
             .await
             .context("no compatible adapter found")?;
 
-        // 4. While an adapter represents the a physical GPU, we also need a logical handle
-        // to this GPU that enforces feature and memory limitations and is responsible for
-        // executing any GPU commands we feed it.
-        // This logical handle is called a "device" and can be requested from the adapter
-        // we created above.
-        // As we have no special requirements at this moment we just request the default
-        // features and limits.
-        // Requesting a device from an adapter returns a tuple containing both the device
-        // and a queue to which we can submit GPU commands.
-        // Note that requesting a device again is an asynchronous operation.
         let (device, queue) = adapter
             .request_device(
                 &wgpu::DeviceDescriptor {
@@ -73,60 +55,63 @@ impl Application {
             )
             .await?;
 
-        // 5. Get the default config for our adapter from the surface, using the size
-        // we got as parameter to our constructor. Make sure the size has a width and
-        // height of at least 1, otherwise creating the surface may fail.
-        // This only returns None if the surface and adapter are incompatible.
-        // As we requested the adapter with `compatible_surface`, this is never the case.
         let surface_config = surface
             .get_default_config(&adapter, size.width.max(1), size.height.max(1))
+            // `get_default_config` only returns None if the surface and adapter are incompatible.
+            // As we requested the adapter with `compatible_surface`, this is never the case.
             .unwrap();
-
-        // 6. Configure the surface using our logical device and the surface config.
         surface.configure(&device, &surface_config);
 
-        // 7. Load the shader source code from `application.wgsl` and create a shader module
-        // on our logical device to which we pass the loaded code as source.
-        // As shader source type, we use WGSL.
-        // You can optionally pass a label that will be used when reporting errors regarding
-        // this particular shader module.
+        // We encapsulate the actual ray tracing renderer into a separate module.
+        // This application module now is only responsible for displaying frames rendered by the
+        // ray tracer to the screen.
+        let scene = Scene::new(&device, Vector3::zero(), size.width, size.height);
+
+        // 1. To be able to display the ray tracer's rendered texture to the screen, we must make
+        // our render pipeline know about it first.
+        // This can be accomplished with bind groups.
+        // Before we can create a bind group, we must describe its layout.
+        //
+        // Task: Create a bind group layout directly on our `device`.
+        // - The bind group layout should have exactly one entry, binding to index 0.
+        // - As we must read from the texture inside our fragment shader to be able to present it,
+        // the bind group layout entry must have the fragment shader stage as visibility.
+        // - The type of our entry is a texture.
+        // - As a fragment shader returns the color with floating point components, we specify
+        // a sample type of float.
+        // - It doesn't have to be filterable as we map one pixel from the scene's texture to exactly
+        // one pixel on the screen.
+        // - Also, we don't want multisampling as our texture only has one layer.
+        // - As we don't bind an array of textures but just a single texture, our count is `None`.
+        // let render_bind_group_layout = ...
+        
+        // 2. After creating the layout for our bind group, we can create the bind group itself.
+        // This is again done using our `device`.
+        // Specify the layout we created above.
+        // As entries, we pass a slice with only one bind group entry, again using binding index 0.
+        // Just like in the previous chapter, texture's are accessed through views.
+        // Our abstraction in `texture.rs` already created the view for us, which we can access as
+        // `scene.texture.view`.
+        // To pass a texture view as resource of a bind group entry, it must be wrapped in the
+        // `wgpu::BindingResource` enum.
+        // let render_bind_group = ...
+
         let shader_src = include_str!("application.wgsl");
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("application shader"),
             source: wgpu::ShaderSource::Wgsl(shader_src.into()),
         });
 
-        // 8. Define the layout for our pipeline by creating a pipeline layout on our device.
-        // Our layout is very basic for now, so it is sufficient to use the PipelineLayoutDescriptor's
-        // default initializer.
+        // 3. We must also include the layout of our bind group in our render pipeline layout
+        // so the render pipeline knows what data to expect.
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("render_pipeline_layout"),
-                bind_group_layouts: &[],
+                // TODO: uncomment after you created your bind group layout
+                // bind_group_layouts: &[&render_bind_group_layout],
                 push_constant_ranges: &[],
+                ..Default::default()
             });
-
-        // 9. Next, create the render pipeline itself on the device.
-        // This requires:
-        // - layout: Our pipeline layout created above.
-        // - vertex: A description of our pipeline's VertexState. This receives our shader module
-        //   and optionally the name of the entry_point function inside that shader module
-        //   As we only have one vertex shader in our code, this can be set to None for
-        //   automatic detection.
-        //   We don't need any buffer and no special compilation options.
-        // - fragment: A description of our pipeline's FragmentState. This receives our shader module
-        //   and optionally the name of the entry_point function inside that shader module
-        //   As we only have one fragment shader in our code, this can be set to None for
-        //   automatic detection.
-        //   Also, we must define the color targets inside our fragment state.
-        //   We only have one color target, which is defined by our surface_config's format,
-        //   and should use a replacement blend (overwriting colors of the previous render)
-        //   as well as write all color components our shaders return.
-        //   We don't need any special compilation options.
-        // - primitive: A description of our pipeline's PrimitiveState. This defines what
-        //   kind of geometric primitive will be used in our render pipeline.
-        //   We use the default primitive, a triangle list.
-        // All other parameters may use their defaults.
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("render_pipeline"),
             layout: Some(&render_pipeline_layout),
@@ -156,73 +141,110 @@ impl Application {
             cache: None,
         });
 
-        // Save these for later use
         Ok(Self {
             surface_config,
             surface,
             device,
             queue,
+            scene,
+            // render_bind_group_layout,
+            // render_bind_group,
             render_pipeline,
+            mouse_down: false,
         })
     }
 
     pub fn resize(&mut self, width: u32, height: u32) {
         log::info!("Resize: {}x{}", width, height);
-
-        // 1. Update our surface_config to the new dimensions.
-        // Note that in rare scenarios, we may receive a width or height
-        // of zero. Ensure the configured surface has a width and height
-        // of at least one, otherwise we will run into validation issues.
         self.surface_config.width = width.max(1);
         self.surface_config.height = height.max(1);
-
-        // 2. Reconfigure our surface using the updated surface_config
         self.surface.configure(&self.device, &self.surface_config);
+
+        // When our window is resized, we now must not only resize the surface we display
+        // our scene on but also the texture our ray tracer renders too.
+        // The resize itself is abstracted into the scene module, which you will touch in a moment.
+        self.scene
+            .resize_texture(&self.device, &self.queue, width.max(1), height.max(1));
+
+        // 4. A texture can not actually be resized, instead a new texture with the desired size
+        // is created inside `Scene::resize_texture`.
+        // This also means that the texture view we passed to our bind group before is not valid
+        // anymore, it's still pointing to the old texture.
+        // Recreate the bind group here (overwriting the current one in `self.render_bind_group`),
+        // using the same arguments as in `Application::new` (`self.render_bind_group_layout` as
+        // layout, `self.scene.texture.view` as texture view).
+        // self.render_bind_group = ...
     }
 
-    pub fn handle_event(
-        &mut self,
-        window: &winit::window::Window,
-        winit_event: &winit::event::WindowEvent,
-    ) -> bool {
-        false
+    pub fn handle_event(&mut self, window: &Window, event: &WindowEvent) -> bool {
+        // We use mouse and keyboard inputs to control our camera.
+        // Feel free to experiment with this if you have time!
+        match event {
+            // On left mouse button down, track mouse movement to control the camera.
+            // On left mouse button up, stop controlling the camera.
+            WindowEvent::MouseInput {
+                state,
+                button: MouseButton::Left,
+                ..
+            } => {
+                self.scene.prev_pointer_pos = None;
+                self.mouse_down = match state {
+                    ElementState::Pressed => true,
+                    ElementState::Released => false,
+                };
+                true
+            }
+            // Mouse movement rotates our arcball.
+            WindowEvent::CursorMoved { position, .. } if self.mouse_down => {
+                self.scene.on_pointer_moved(
+                    &self.queue,
+                    CameraOperation::Rotate,
+                    (position.x as f32, position.y as f32),
+                );
+                true
+            }
+            // The mouse wheel controls the camera zoom.
+            WindowEvent::MouseWheel { delta, .. } => {
+                let delta = match delta {
+                    MouseScrollDelta::LineDelta(_horz, vert) => *vert,
+                    MouseScrollDelta::PixelDelta(pos) => pos.y as f32,
+                };
+                self.scene.on_zoom(&self.queue, delta);
+                true
+            }
+            // When the space bar is pressed, reset the camera.
+            WindowEvent::KeyboardInput { event, .. }
+                if event.physical_key == PhysicalKey::Code(KeyCode::Space) =>
+            {
+                self.scene.reset_camera(&self.queue);
+                true
+            }
+            _ => false,
+        }
     }
 
-    pub fn render(&mut self, window: &winit::window::Window) -> Result<(), wgpu::SurfaceError> {
-        // Relevant wgpu types for this method:
-        // - SurfaceTexture, Texture, TextureView
-        // - CommandEncoder, CommandEncoderDescriptor
-        // - RenderPass, RenderPassDescriptor
-        // - RenderPassColorAttachment, Operations, LoadOp, StoreOp, Color
-
-        // 1. To render something to the screen, we must first request the current
-        // texture from our surface.
+    pub fn render(&mut self, window: &Window) -> Result<(), wgpu::SurfaceError> {
         let frame = self.surface.get_current_texture()?;
-
-        // 2. A texture itself cannot be used as render target.
-        // We must create a view from this texture that then contains the metadata
-        // our render pipeline needs to render to it.
         let view = &frame
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
 
-        // 3. All commands to be enqueued to our GPU's queue must first be encoded
-        // so they are compatible with our logical device.
-        // For this, we create a command encoder using our device.
         let mut encoder = self
             .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
                 label: Some("encoder"),
             });
 
-        // 4. Defining rendering commands for a GPU happens in form of a render pass.
-        // We create a render pass by "beginning" it on the command encoder.
-        // To actually get something out of the render pass, we give it a slice of
-        // color attachments to render to (in our case, just one).
-        // This color attachment receives the view we created for our surface texture earlier.
-        // We then tell it what operations (ops) to perform on this view:
-        // - On load, clear the surface texture using a black color
-        // - On store, overwrite the contents of the surface texture (simply called "Store")
+        // A wgpu debug group helps us identify in which part of our application
+        // errors occured, and can also be used to identify parts of our rendering
+        // process in a graphical debugger such as RenderDoc or Xcode.
+        // See https://github.com/gfx-rs/wgpu/wiki/Debugging-wgpu-Applications
+        // for more information.
+        encoder.push_debug_group("render scene");
+        self.scene.render(&mut encoder);
+        encoder.pop_debug_group();
+
+        encoder.push_debug_group("display");
         let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: Some("rpass"),
             color_attachments: &[Some(wgpu::RenderPassColorAttachment {
@@ -236,26 +258,24 @@ impl Application {
             depth_stencil_attachment: None,
             ..Default::default()
         });
-        
-        // 5. To let the render pass know of the structure of our pipeline, such as
-        // shaders, or geometric primitives, set its pipeline to the render pipeline
-        // we created in our constructor.
         rpass.set_pipeline(&self.render_pipeline);
 
-        // 6. Tell the render pass to draw six vertices (must be passed as a range 0 to 6)
-        // for one instance (again, as a range 0 to 1).
-        // Instancing will not be covered in this workshop.
+        // 5. Our render pipline already knows about the required bindings thanks to
+        // the bind group layout we included in the render pipeline layout.
+        // But as you may have noticed, we haven't actually used the render bind group
+        // itself anywhere yet.
+        // As a bind group contains data used for rendering, and a render pass
+        // requires all rendering specific data, we must set the render passes
+        // bind group to our render bind group first before performing the draw call.
+        // Set the bind group to index 0, as that is the index we specified in our bind
+        // group layout, without any offsets (empty slice).
+        // rpass.set...
+
         rpass.draw(0..6, 0..1);
-
-        // 7. Before finishing our command encoder, we must drop the
-        // render pass so it knows it is complete.
         drop(rpass);
+        encoder.pop_debug_group();
 
-        // 8. Finish the command encoder, returning a command buffer.
-        // Then, submit the command buffer to our GPU queue.
         self.queue.submit(Some(encoder.finish()));
-
-        // 9. Present the frame (our SurfaceTexture)
         frame.present();
 
         Ok(())
