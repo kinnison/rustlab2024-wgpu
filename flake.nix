8ec6be97d083c8045a78a26589b6deb4b2047917
Niklas Korz <niklas@niklaskorz.de>
2024-11-08 10:19:05 +0100

    Add NixOS shell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = import ./shell.nix {
+          pkgs = nixpkgs.legacyPackages.${system};
+        };
+      });
+    };
+}
+
